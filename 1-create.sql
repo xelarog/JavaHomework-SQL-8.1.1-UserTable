8fb8bb41fc458cfa72f50df5e628434beb970f14
xelarog <stollprog@mail.ru>
2022-11-12 00:22:51 +0300

    Update 1-create.sql

--- 1-create.sql
+++ 1-create.sql
@@ -2,7 +2,7 @@ CREATE TABLE PERSONS (
     name varchar NOT NULL ,
     surname varchar NOT NULL,
     age int NOT NULL,
-    phone_number int,
+    phone_number varchar,
     city_of_living varchar,
     PRIMARY KEY(name, surname, age)
 );
